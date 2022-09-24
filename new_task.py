275e9522ba42ba880cf3e0c0b53e297fa7bd554a
shraddha malviya <110804368+shraddhavmalviya@users.noreply.github.com>
2022-09-24 11:20:09 +0530

    Update new_task.py

--- new_task.py
+++ new_task.py
@@ -8,4 +8,4 @@ df = pd.read_csv("aartiiiicsv.csv")
 df = pd.DataFrame(df)
 cnx = sqlalchemy.create_engine('mysql+pymysql://root:12345@localhost/test')
 df.to_sql('data_table1', con=cnx)
-cnx.close
+cnx.close()
