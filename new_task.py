3329a9a8ba6c7da3f571ea1f8d7357d0a2b9d030
shraddhavmalviya <shradha.v@zecdata.com>
2022-09-24 10:54:48 +0530

    Add New CHanges

---
+++ new_task.py
@@ -0,0 +1,10 @@
+# from sqlalchemy import create_engine
+import pymysql.cursors
+import pandas as pd
+import sqlalchemy
+import pymysql
+pymysql.install_as_MySQLdb()
+df = pd.read_csv("aartiiiicsv.csv")
+df = pd.DataFrame(df)
+cnx = sqlalchemy.create_engine('mysql+pymysql://root:12345@localhost/test')
+df.to_sql('data_table1', con=cnx)
